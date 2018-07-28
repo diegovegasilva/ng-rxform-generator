937a9faee2d1e26e3f805e66ee69641a6afea68b
Diego Vega <diegovegasilva@gmail.com>
2018-07-28 13:30:42 +0200

    remove components from public api

--- projects/ng-rxform-generator/src/public_api.ts
+++ projects/ng-rxform-generator/src/public_api.ts
@@ -2,8 +2,6 @@
  * Public API Surface of ng-rxform-generator
  */
 
-export * from './lib/formGenerator/ng-rxform-generator.component';
 export * from './lib/formGenerator/ng-rxform-generator.module';
-export * from './lib/formMaterialGenerator/ng-rxform-material-generator.component';
 export * from './lib/formMaterialGenerator/ng-rxform-material-generator.module';
 export * from './lib/classes';
